2fd00571b3a1bef9fb0a746fdfced50bef71fba3
Frank Jennings <frank@jenningsnet.net>
2026-02-02 13:32:32 -0800

    Update Wallpaper and start themes

--- dotfiles/.config/hypr/scripts/wallpaper-picker.sh
+++ dotfiles/.config/hypr/scripts/wallpaper-picker.sh
@@ -3,8 +3,10 @@
 WALLPAPER_DIR="$HOME/Pictures/wallpaper"
 
 # Select an image from the directory using ROFI
-SELECTED_WALL=$(ls "$WALLPAPER_DIR" | rofi -dmenu -p "Select WallPaper")
+SELECTED_WALL=$(ls "$WALLPAPER_DIR" | rofi -dmenu -i -show-icons -p "Select WallPaper")
 
 if [ -n "$SELECTED_WALL" ]; then
-    swww img "$WALLPAPER_DIR/$SELECTED_WALL" --transition-type=random
+    FULL_PATH="$WALLPAPER_DIR/$SELECTED_WALL"
+    swww img "$FULL_PATH" --transition-type=random
+    wal -i "$FULL_PATH" -n -q
 fi
